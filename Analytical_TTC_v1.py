1d03101b052e2ddc635d41dcaa2ec58a0d14176d
chrisp33 <82350748+chrisp33@users.noreply.github.com>
2021-04-27 10:40:39 -0400

    Uploaded files

---
+++ Analytical_TTC_v1.py
@@ -0,0 +1,56 @@
+import streamlit as st
+
+
+"""
+Author: Christopher Pulliam 
+# Toxicological Threshold Calculator 
+
+### Fill in the blanks and the Analytical TTC will be returned
+"""
+
+"""
+## GPS Threshold
+"""
+ttc = st.number_input('Input GPS threshold in mg/g', format = '%.3f')
+
+'''## Analytical Concentration in mg/mL'''
+
+anal_conc = st.number_input("Input Analytical Concentration", 0.0,)
+
+"""
+## Select Extraction Ratio or Extraction Percent
+"""
+ext_val_type = st.radio('Select', ['Extraction Ratio', 'Extraction Percent'])
+
+if ext_val_type == 'Extraction Ratio':
+	
+	ext_ratio = st.number_input(f'Input {ext_val_type}', format = '%.3f', key = 1)
+
+	st.markdown(f'## Analytical TTC based on {anal_conc} mg/mL:')
+	try:
+		val = ttc*ext_ratio/1000/anal_conc*100
+	except ZeroDivisionError: #addresses initial conc = 0.00
+		val = 0
+	st.title('{:.3f} mg/mL'.format(val))
+
+
+else: 
+	ext_percent = st.number_input(f'Input {ext_val_type}', None, format = '%.3f', key = 2)
+
+	
+	try:
+		ext_ratio = 100/ext_percent
+	except ZeroDivisionError: #Eliminates the need for a real default value. 
+		ext_ratio = 0
+		
+	st.markdown(f'## Analytical TTC based on {anal_conc} mg/mL:')
+
+	val = ttc*ext_ratio/1000/anal_conc*100
+	# if float()
+	st.title('{:.3f} mg/mL'.format(round(val,2)))
+
+
+
+# if ext_val_type ==
+
+"""version 1.1"""
